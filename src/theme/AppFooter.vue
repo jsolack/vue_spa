3d32dc9f0c53e47b511ec9dea0fe37062094f0dc
Matt Castle <mattcc82@gmail.com>
2018-02-20 12:38:04 -0500

    fix footer / vue transition

--- src/theme/AppFooter.vue
+++ src/theme/AppFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer">
     <div class="container">
-      <div class="content has-text-centered">
+      <div class="has-text-centered">
         Footer!
         <a href="#">Link</a>
       </div>
--- src/theme/Layout.vue
+++ src/theme/Layout.vue
@@ -1,15 +1,17 @@
 <template v-cloak>
   <div id="app">
-    <app-header></app-header>
-    <hero v-if="['notfound'].indexOf($route.name) === -1"></hero>
-    <section class="main-section section">
-      <div class="content">
-        <!-- Vue router will load in the Category component on the '/' path -->
-        <transition appear name="slide-fade">
-          <router-view v-bind:key="$route.params.id"></router-view>
-        </transition>
-      </div>
-    </section>
+    <div class="main-section">
+      <app-header></app-header>
+      <hero v-if="['notfound'].indexOf($route.name) === -1"></hero>
+      <section>
+        <div class="content" ref="content">
+          <transition appear name="slide-fade" mode="out-in">
+          <!-- Vue router will load in the Category component on the '/' path -->
+            <router-view v-bind:key="$route.params.id" class="data-items"></router-view>
+          </transition>
+        </div>
+      </section>
+    </div>
     <app-footer></app-footer>
   </div>
 </template>
@@ -30,22 +32,49 @@
   $link: #658396;
   @import '~bulma';
 
-  [v-cloak] {
-    display: none;
+  /* css rules */
+  * {
+    box-sizing: border-box;
+  }
+  html, body {
+    height: 100%;
+  }
+  #app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+  nav {
+    position: absolute;
+  }
+  section {
+    flex: 1 0 auto;
   }
   .main-section {
     padding: 0;
+    flex: 1;
+  }
+  footer.footer {
+    flex-shrink: 0;
+    height: 10vh;
+  }
+  [v-cloak] {
+    display: none;
   }
   .columns{
     flex-wrap: wrap;
   }
+  .data-items {
+    height: 100%;
+    min-height: 400px;
+  }
   /* Enter and leave animations can use different */
   /* durations and timing functions.              */
   .slide-fade-enter-active {
-    transition: all .8s ease;
+    transition: all .3s ease;
   }
   .slide-fade-leave-active {
-    transition: all 0s ease-in-out;
+    transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
   }
   .slide-fade-enter, .slide-fade-leave-to
   /* .slide-fade-leave-active below version 2.1.8 */ {
