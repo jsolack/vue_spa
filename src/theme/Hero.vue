87d6a0a2eb49b2c38d346fee085f43d2e701133a
Matt Castle <mattcc82@gmail.com>
2018-02-27 17:15:21 -0500

    quick dirty auth check

--- src/theme/Hero.vue
+++ src/theme/Hero.vue
@@ -6,19 +6,24 @@
           {{main_title}}
         </h1>
         <h2 class="subtitle">
-          {{main_subtitle}}
+          <span v-if="isAuthenticated">{{main_subtitle}}</span>
+          <span v-else>Please log in</span>
         </h2>
       </div>
     </div>
   </section>
 </template>
 <script>
+import { mapGetters } from 'vuex'
 export default {
   data () {
     return {
       main_title: 'Data Tools',
       main_subtitle: 'Check it out'
     }
+  },
+  computed: {
+    ...mapGetters(['isAuthenticated'])
   }
 }
 </script>
